49d971f0036872727a98caae21aa0eb8f26690fc
root <root@SH-lihang.HQ.CORP>
2020-08-04 14:59:52 +0800

    test5

--- index.js
+++
@@ -1,6 +0,0 @@
-const http = require('http')
-http.createServer((req, res) => {
-	res,end('i am web')
-}).listen(3000, res => {
-	console.log('server start')
-})
--- test.sh
+++
@@ -1,4 +0,0 @@
-export DOCKER_USERNAME=mrrs878
-export DOCKER_PASSWORD=@Qq879343544
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
